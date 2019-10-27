09704ef89b7a45e1b42d27d263dbf02a6b1bb053
Vkadel <35178154+Vkadel@users.noreply.github.com>
2019-10-26 23:53:37 -0400

    Got role selection screen out

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:bob_mobile/data_type/user.dart';
 import 'package:bob_mobile/firestore.dart';
 import 'package:bob_mobile/provider.dart';
 import 'package:bob_mobile/qanda.dart';
+import 'package:bob_mobile/select_role_page.dart';
 import 'package:bob_mobile/validators.dart';
 import 'package:bob_mobile/widgets/google_signin_button.dart';
 import 'package:bob_mobile/widgets/loading_indicator_full_screen.dart';
@@ -327,96 +328,6 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: Text('Welcome'),
-    );
-  }
-}
-
-class SelectRolePage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return new _SelectRoleState();
-  }
-}
-
-class _SelectRoleState extends State<SelectRolePage> {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Builder(
-        builder: (BuildContext context) {
-          child:
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(Constants().select_your_role_page_title),
-              bottom: TabBar(
-                tabs: <Widget>[
-                  new SvgPicture.asset(
-                    Constants.myAvatars.elementAt(0).small_icon,
-                    width: 50,
-                    color: Constants.icon_Colors,
-                  ),
-                  new SvgPicture.asset(
-                    Constants.myAvatars.elementAt(1).small_icon,
-                    width: 50,
-                    color: Constants.icon_Colors,
-                  ),
-                  new SvgPicture.asset(
-                    Constants.myAvatars.elementAt(2).small_icon,
-                    width: 50,
-                    color: Constants.icon_Colors,
-                  ),
-                ],
-              ),
-            ),
-            body: TabBarView(
-              children: <Widget>[
-                myScrollableWindow(context, heroTab(0, context)),
-                myScrollableWindow(context, heroTab(1, context)),
-                myScrollableWindow(context, heroTab(2, context))
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  /*select_hero(int hero_id) {
-    Quanda.of(context).myUser.role = hero_id;
-    Provider.of(context).fireBase.setUpHero(
-        Provider.of(context).auth.getLastUserLoged(),
-        Quanda.of(context).myUser);
-  }*/
-
-  Widget heroTab(int location, BuildContext context) {
-    final String contractText =
-        Constants.myAvatars.elementAt(location).contract_button;
-    final item_pressed = location + 1;
-    return Column(
-      children: <Widget>[
-        Image(
-          width: 300,
-          image:
-              AssetImage(Constants.myAvatars.elementAt(location).asset_Large),
-        ),
-        FlatButton(
-          onPressed: () {
-            /*   select_hero(DefaultTabController.of(context).index + 1);*/
-            Quanda.of(context).myUser.role =
-                DefaultTabController.of(context).index + 1;
-            Provider.of(context).fireBase.setUpHero(
-                Provider.of(context).auth.getLastUserLoged(),
-                Quanda.of(context).myUser);
-            print(
-                'This is the index ${DefaultTabController.of(context).index}');
-          },
-          child: Row(
-            children: <Widget>[Text('$contractText')],
-          ),
-        ),
-        TextFormattedBody(Constants.myAvatars.elementAt(location).story),
-      ],
     );
   }
 }
